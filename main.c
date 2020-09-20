dd845f0278feb90435de52e7c5a7442fcbfd5132
rahmagehad <rahmagehad1@gmail.com>
2020-09-21 01:37:18 +0200

    me

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    #include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+float dollar,egp;
+printf("USD to EGP converter \nEnter a value \n");
+scanf("%f", &dollar);
+egp = dollar*15.75;
+printf("That is equivalent to %0.2f in egp \n", egp);
+return 0;
+}
+
+}
